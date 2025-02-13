c7d08b1f833a97c0cd0e8e4d58287b9fcf7ddfa6
Quinten Lisowe <quinten.lisowe@thelisowe.com>
2025-02-12 19:15:54 -0600

    Fixed getkeys to directly read controller inputs

--- rl_utils/getkeys.py
+++ rl_utils/getkeys.py
@@ -1,75 +1,95 @@
-import keyboard
-import mouse
-import pygame
-
-pygame.display.init()
-pygame.joystick.init()
-
-joystick = pygame.joystick.Joystick(0)
-joystick.init()
-buttons = joystick.get_numbuttons()
-hats = joystick.get_numhats()
-axes = joystick.get_numaxes()
-
-# keyboard_inputs, char, mouse_inputs
-keyboard_inputs = ["shift", "alt", "ctrl"]
-mouse_inputs = ["left", "right"]
-
-for char in "abcdefghijklmnopqrstuvwxyz 123456789,.'":
-    keyboard_inputs.append(char)
+import evdev
+from evdev import ecodes
+import time
+import sys
 
 
-def get_inputs():
-    inputs_event = []
+class InputMonitor:
+    def __init__(self):
+        self.current_keys = set()
+        self.axis_states = {
+            ecodes.ABS_X: 0,  # Left stick X
+            ecodes.ABS_Y: 0,  # Left stick Y
+            ecodes.ABS_RX: 0,  # Right stick X
+            ecodes.ABS_RY: 0,  # Right stick Y
+            ecodes.ABS_HAT0X: 0,  # D-pad X
+            ecodes.ABS_HAT0Y: 0,  # D-pad Y
+        }
+        self.devices = self._get_input_devices()
 
-    for button in keyboard_inputs:
-        if keyboard.is_pressed(button):
-            inputs_event.append(button)
-    for button in mouse_inputs:
-        if mouse.is_pressed(button=button):
-            inputs_event.append(button)
+        print("Detected input devices:")
+        for i, dev in enumerate(self.devices):
+            print(f" {i + 1}. {dev.name} (Path: {dev.path})")
 
-    # Joystick input for a logitech Gamepad F310
-    for i in range(buttons):
-        pygame.event.pump()
-        button = joystick.get_button(i)
-        if i == 2 and button == 1:  # X button pressed
-            inputs_event.append("shift")
+    def _get_input_devices(self):
+        devices = []
+        for device_path in evdev.list_devices():
+            try:
+                dev = evdev.InputDevice(device_path)
+                # Include all input devices since controllers might not be classified as keyboards/mice
+                devices.append(dev)
+            except:
+                continue
+        return devices
 
-    for i in range(axes):
-        pygame.event.pump()
-        axis = joystick.get_axis(i)
-        if i == 0 and axis < -0.2:  # Left stick going left
-            inputs_event.append("a")
-        if i == 0 and axis > 0.2:  # Left stick going right
-            inputs_event.append("d")
-        if i == 1 and axis > 0.2:  # Left stick going down
-            inputs_event.append("s")
-        elif i == 2 and axis > 0.15 and "s" not in inputs_event:  # LT is pulled
-            inputs_event.append("s")
-        if i == 1 and axis < -0.2:  # Left stick going up
-            inputs_event.append("w")
-        elif i == 2 and axis < -0.15 and "w" not in inputs_event:  # RT is pulled
-            inputs_event.append("w")
+    def _get_key_name(self, key_event):
+        """Convert keycode tuple to string if needed"""
+        if isinstance(key_event.keycode, tuple):
+            return "/".join(key_event.keycode)
+        return key_event.keycode
 
-    # for i in range(hats):
-    #     pygame.event.pump()
-    #     hat = joystick.get_hat(i)
-    #     if i == 0 and hat == (0,1): # Up dpad
-    #         inputs_event.append("1")
-    #     if i == 0 and hat == (-1,0): # Left dpad
-    #         inputs_event.append("2")
-    #     if i == 0 and hat == (1,0): # Right dpad
-    #         inputs_event.append("3")
-    #     if i == 0 and hat == (0,-1): # Left dpad
-    #         inputs_event.append("4")
+    def monitor(self):
+        try:
+            print("\nMonitoring input (Press CTRL+C to exit)...")
+            for device in self.devices:
+                try:
+                    device.grab()
+                    print(f"Grabbed device: {device.name}")
+                except Exception as e:
+                    print(f"Couldn't grab {device.name}: {str(e)}")
 
-    for i in range(buttons):
-        pygame.event.pump()
-        button = joystick.get_button(i)
-        if i == 0 and button == 1:  # A button pressed
-            inputs_event.append("left")
-        if i == 1 and button == 1:  # B button pressed
-            inputs_event.append("right")
+            while True:
+                for device in self.devices:
+                    try:
+                        events = device.read()
+                        if not events:
+                            continue
 
-    return inputs_event
+                        for event in events:
+                            if event.type == ecodes.EV_KEY:
+                                key_event = evdev.categorize(event)
+                                key_name = self._get_key_name(key_event)
+                                if key_event.keystate == key_event.key_down:
+                                    self.current_keys.add(key_name)
+                                elif key_event.keystate == key_event.key_up:
+                                    self.current_keys.discard(key_name)
+
+                            elif event.type == ecodes.EV_ABS:
+                                # Handle analog sticks and d-pad
+                                if event.code in self.axis_states:
+                                    self.axis_states[event.code] = event.value
+
+                    except BlockingIOError:
+                        continue
+
+                # Get formatted axis values
+                axis_display = (
+                    f"L: ({self.axis_states[ecodes.ABS_X]}, {self.axis_states[ecodes.ABS_Y]}) "
+                    f"R: ({self.axis_states[ecodes.ABS_RX]}, {self.axis_states[ecodes.ABS_RY]}) "
+                    f"D-pad: ({self.axis_states[ecodes.ABS_HAT0X]}, {self.axis_states[ecodes.ABS_HAT0Y]})"
+                )
+
+                output = f"Keys: {sorted(self.current_keys)} | {axis_display}"
+                print(f"\r{output.ljust(120)}", end="", flush=True)
+                time.sleep(0.05)
+
+        except KeyboardInterrupt:
+            print("\nMonitoring stopped.")
+            for device in self.devices:
+                device.ungrab()
+            sys.exit(0)
+
+
+if __name__ == "__main__":
+    monitor = InputMonitor()
+    monitor.monitor()
